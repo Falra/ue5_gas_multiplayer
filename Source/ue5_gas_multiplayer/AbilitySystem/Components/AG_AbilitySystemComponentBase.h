ddd78289bb74ee8464d542cf9ab9c49416a95dd7
Valerii Diachenko <leriy.dyachenko@gmail.com>
2023-03-23 20:37:30 +0200

    #12 Add blank base ability component

---
+++ Source/ue5_gas_multiplayer/AbilitySystem/Components/AG_AbilitySystemComponentBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystemComponent.h"
+#include "AG_AbilitySystemComponentBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE5_GAS_MULTIPLAYER_API UAG_AbilitySystemComponentBase : public UAbilitySystemComponent
+{
+    GENERATED_BODY()
+    
+};
