901593583b2b6a479a35ac279fcb49e05ffed004
Valerii Diachenko <leriy.dyachenko@gmail.com>
2023-03-24 16:58:42 +0200

    #12 Add health/max health attributes

--- Source/ue5_gas_multiplayer/AbilitySystem/AttributeSets/AG_AttributeSetBase.cpp
+++ Source/ue5_gas_multiplayer/AbilitySystem/AttributeSets/AG_AttributeSetBase.cpp
@@ -2,3 +2,16 @@
 
 
 #include "AG_AttributeSetBase.h"
+
+void UAG_AttributeSetBase::PostGameplayEffectExecute(const FGameplayEffectModCallbackData& Data)
+{
+    Super::PostGameplayEffectExecute(Data);
+}
+
+void UAG_AttributeSetBase::OnRep_Health()
+{
+}
+
+void UAG_AttributeSetBase::OnRep_MaxHealth()
+{
+}
--- Source/ue5_gas_multiplayer/AbilitySystem/AttributeSets/AG_AttributeSetBase.h
+++ Source/ue5_gas_multiplayer/AbilitySystem/AttributeSets/AG_AttributeSetBase.h
@@ -3,16 +3,38 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "AbilitySystemComponent.h"
 #include "AttributeSet.h"
 #include "AG_AttributeSetBase.generated.h"
 
-/**
- * 
- */
+#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
+   GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+   GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+   GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+   GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
+
 UCLASS()
 class UE5_GAS_MULTIPLAYER_API UAG_AttributeSetBase : public UAttributeSet
 {
     GENERATED_BODY()
 
+public:
 
+    UPROPERTY(BlueprintReadOnly, Category = "Health", ReplicatedUsing = OnRep_Health)
+    FGameplayAttributeData Health;
+    ATTRIBUTE_ACCESSORS(UAG_AttributeSetBase, Health)
+    
+    UPROPERTY(BlueprintReadOnly, Category = "Health", ReplicatedUsing = OnRep_MaxHealth)
+    FGameplayAttributeData MaxHealth;
+    ATTRIBUTE_ACCESSORS(UAG_AttributeSetBase, MaxHealth)
+
+protected:
+
+    virtual void PostGameplayEffectExecute(const FGameplayEffectModCallbackData& Data) override;
+    
+    UFUNCTION()
+    virtual void OnRep_Health();
+
+    UFUNCTION()
+    virtual void OnRep_MaxHealth();
 };
