e1a964127ddac1a94f60c870a9a28b2da3720c68
Valerii Diachenko <leriy.dyachenko@gmail.com>
2023-03-29 19:59:11 +0300

    #15 Add blank character movement component

---
+++ Source/ue5_gas_multiplayer/ActorComponents/AG_CharacterMovementComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "AG_CharacterMovementComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE5_GAS_MULTIPLAYER_API UAG_CharacterMovementComponent : public UCharacterMovementComponent
+{
+    GENERATED_BODY()
+
+};
