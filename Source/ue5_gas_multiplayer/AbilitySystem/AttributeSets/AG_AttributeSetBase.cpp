5a3b0f16fa03a72db9b44643a04501a9507e531e
Valerii Diachenko <leriy.dyachenko@gmail.com>
2023-03-29 20:13:38 +0300

    #15 Add attributes for stamina and movement speed

--- Source/ue5_gas_multiplayer/AbilitySystem/AttributeSets/AG_AttributeSetBase.cpp
+++ Source/ue5_gas_multiplayer/AbilitySystem/AttributeSets/AG_AttributeSetBase.cpp
@@ -26,9 +26,27 @@ void UAG_AttributeSetBase::OnRep_MaxHealth(const FGameplayAttributeData& OldMaxH
     GAMEPLAYATTRIBUTE_REPNOTIFY(UAG_AttributeSetBase, MaxHealth, OldMaxHealth);
 }
 
+void UAG_AttributeSetBase::OnRep_Stamina(const FGameplayAttributeData& OldStamina)
+{
+    GAMEPLAYATTRIBUTE_REPNOTIFY(UAG_AttributeSetBase, Stamina, OldStamina);
+}
+
+void UAG_AttributeSetBase::OnRep_MaxStamina(const FGameplayAttributeData& OldMaxStamina)
+{
+    GAMEPLAYATTRIBUTE_REPNOTIFY(UAG_AttributeSetBase, MaxStamina, OldMaxStamina);
+}
+
+void UAG_AttributeSetBase::OnRep_MaxMovementSpeed(const FGameplayAttributeData& OldMaxMovementSpeed)
+{
+    GAMEPLAYATTRIBUTE_REPNOTIFY(UAG_AttributeSetBase, MaxMovementSpeed, OldMaxMovementSpeed);
+}
+
 void UAG_AttributeSetBase::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
 {
     Super::GetLifetimeReplicatedProps(OutLifetimeProps);
     DOREPLIFETIME_CONDITION_NOTIFY(UAG_AttributeSetBase, Health, COND_None, REPNOTIFY_Always);
     DOREPLIFETIME_CONDITION_NOTIFY(UAG_AttributeSetBase, MaxHealth, COND_None, REPNOTIFY_Always);
+    DOREPLIFETIME_CONDITION_NOTIFY(UAG_AttributeSetBase, Stamina, COND_None, REPNOTIFY_Always);
+    DOREPLIFETIME_CONDITION_NOTIFY(UAG_AttributeSetBase, MaxStamina, COND_None, REPNOTIFY_Always);
+    DOREPLIFETIME_CONDITION_NOTIFY(UAG_AttributeSetBase, MaxMovementSpeed, COND_None, REPNOTIFY_Always);
 }
--- Source/ue5_gas_multiplayer/AbilitySystem/AttributeSets/AG_AttributeSetBase.h
+++ Source/ue5_gas_multiplayer/AbilitySystem/AttributeSets/AG_AttributeSetBase.h
@@ -28,6 +28,18 @@ public:
     FGameplayAttributeData MaxHealth;
     ATTRIBUTE_ACCESSORS(UAG_AttributeSetBase, MaxHealth)
 
+    UPROPERTY(BlueprintReadOnly, Category = "Stamina", ReplicatedUsing = OnRep_Stamina)
+    FGameplayAttributeData Stamina;
+    ATTRIBUTE_ACCESSORS(UAG_AttributeSetBase, Stamina)
+    
+    UPROPERTY(BlueprintReadOnly, Category = "Stamina", ReplicatedUsing = OnRep_MaxStamina)
+    FGameplayAttributeData MaxStamina;
+    ATTRIBUTE_ACCESSORS(UAG_AttributeSetBase, MaxStamina)
+
+    UPROPERTY(BlueprintReadOnly, Category = "MovementSpeed", ReplicatedUsing = OnRep_MaxMovementSpeed)
+    FGameplayAttributeData MaxMovementSpeed;
+    ATTRIBUTE_ACCESSORS(UAG_AttributeSetBase, MaxMovementSpeed)
+
 protected:
 
     virtual void PostGameplayEffectExecute(const FGameplayEffectModCallbackData& Data) override;
@@ -37,4 +49,13 @@ protected:
 
     UFUNCTION()
     virtual void OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth);
+
+    UFUNCTION()
+    virtual void OnRep_Stamina(const FGameplayAttributeData& OldStamina);
+
+    UFUNCTION()
+    virtual void OnRep_MaxStamina(const FGameplayAttributeData& OldMaxStamina);
+
+    UFUNCTION()
+    virtual void OnRep_MaxMovementSpeed(const FGameplayAttributeData& OldMaxMovementSpeed);
 };
