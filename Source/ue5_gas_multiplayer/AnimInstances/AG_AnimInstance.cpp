51f4e5c0c6608a9888b6343abbae48eb1f73dff8
Valerii Diachenko <leriy.dyachenko@gmail.com>
2023-04-04 20:05:29 +0300

    #17 Add blank anim instance class

---
+++ Source/ue5_gas_multiplayer/AnimInstances/AG_AnimInstance.cpp
@@ -0,0 +1,4 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AnimInstances/AG_AnimInstance.h"
---
+++ Source/ue5_gas_multiplayer/AnimInstances/AG_AnimInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "AG_AnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE5_GAS_MULTIPLAYER_API UAG_AnimInstance : public UAnimInstance
+{
+    GENERATED_BODY()
+
+};
