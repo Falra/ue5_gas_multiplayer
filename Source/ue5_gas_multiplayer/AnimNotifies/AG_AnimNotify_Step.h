76ee52693c1fadd5d4c7701e27044a16319ae66d
Valerii Diachenko <leriy.dyachenko@gmail.com>
2023-04-05 20:26:11 +0300

    #19 Add blank step notify class

---
+++ Source/ue5_gas_multiplayer/AnimNotifies/AG_AnimNotify_Step.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotify.h"
+#include "AG_AnimNotify_Step.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE5_GAS_MULTIPLAYER_API UAG_AnimNotify_Step : public UAnimNotify
+{
+    GENERATED_BODY()
+
+};
