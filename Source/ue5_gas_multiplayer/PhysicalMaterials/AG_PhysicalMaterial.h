7f28aa546d8de9f8960d7478e7ac7041e77e77c7
Valerii Diachenko <leriy.dyachenko@gmail.com>
2023-04-05 20:11:59 +0300

    #18 Add blank phys mat class

---
+++ Source/ue5_gas_multiplayer/PhysicalMaterials/AG_PhysicalMaterial.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PhysicalMaterials/PhysicalMaterial.h"
+#include "AG_PhysicalMaterial.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE5_GAS_MULTIPLAYER_API UAG_PhysicalMaterial : public UPhysicalMaterial
+{
+    GENERATED_BODY()
+
+};
