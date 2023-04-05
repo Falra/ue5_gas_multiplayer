403563da9ae358b2227480da26cbf52e86232fe5
Valerii Diachenko <leriy.dyachenko@gmail.com>
2023-04-05 21:11:10 +0300

    #19 Implement trace for footstep (p.2 sound and debug)

--- Source/ue5_gas_multiplayer/ActorComponents/AG_FootstepComponent.cpp
+++ Source/ue5_gas_multiplayer/ActorComponents/AG_FootstepComponent.cpp
@@ -3,7 +3,15 @@
 #include "ActorComponents/AG_FootstepComponent.h"
 
 #include "ue5_gas_multiplayerCharacter.h"
+#include "Kismet/GameplayStatics.h"
 #include "PhysicalMaterials/AG_PhysicalMaterial.h"
+
+static TAutoConsoleVariable<int32> CVarShowFootsteps(
+    TEXT("ShowDedugFootsteps"),
+    0,
+    TEXT("Show debug footsteps"),
+    ECVF_Cheat
+);
 
 UAG_FootstepComponent::UAG_FootstepComponent()
 {
@@ -27,26 +35,46 @@ void UAG_FootstepComponent::HandleFootstep(EFoot Foot)
     {
         return;
     }
-    FHitResult HitResult;
+    const int32 ShowFootsteps = CVarShowFootsteps.GetValueOnAnyThread();
+
     FVector SocketLocation = Mesh->GetSocketLocation(Foot == EFoot::Left ? LeftSocketName : RightSocketName);
     FVector Location = SocketLocation + FVector::UpVector * 20;
+    FVector EndLocation = Location + FVector::UpVector * -50.0f;
 
     FCollisionQueryParams QueryParams;
     QueryParams.bReturnPhysicalMaterial = true;
     QueryParams.AddIgnoredActor(Character);
 
-    if (GetWorld()->LineTraceSingleByChannel(HitResult, Location,
-        Location + FVector::UpVector * - 50.0f, ECC_WorldStatic, QueryParams))
+    if (FHitResult HitResult; GetWorld()->LineTraceSingleByChannel(HitResult, Location,
+        EndLocation, ECC_WorldStatic, QueryParams))
     {
         if (HitResult.bBlockingHit)
         {
+            if (ShowFootsteps > 0)
+            {
+                DrawDebugLine(GetWorld(), Location, EndLocation, FColor::Green, false, 4.0f, 0, 1.0f);
+            }
             return;
         }
-        auto* Material = Cast<UAG_PhysicalMaterial>(HitResult.PhysMaterial.Get());
-        if (!Material)
+        if (auto* Material = Cast<UAG_PhysicalMaterial>(HitResult.PhysMaterial.Get()))
         {
-            return;
+            UGameplayStatics::PlaySoundAtLocation(this, Material->FootstepSound, SocketLocation, 1.0f);
+            if (ShowFootsteps > 0)
+            {
+                DrawDebugString(GetWorld(), HitResult.ImpactPoint, *Material->GetName(), nullptr, FColor::White, 4.0f);
+            }
         }
-        
+        if (ShowFootsteps > 0)
+        {
+            DrawDebugSphere(GetWorld(), HitResult.ImpactPoint, 16, 16, FColor::Red, false, 4.0f);
+        }
+    }
+    else
+    {
+        if (ShowFootsteps > 0)
+        {
+            DrawDebugLine(GetWorld(), Location, EndLocation, FColor::Green, false, 4.0f, 0, 1.0f);
+            DrawDebugSphere(GetWorld(), EndLocation, 16, 16, FColor::Red, false, 4.0f);
+        }
     }
 }
