20981f8f94c420c4821b89e7f1f35970549681e7
Valerii Diachenko <leriy.dyachenko@gmail.com>
2023-04-15 15:19:10 +0300

    #21 Add blank basic ability class

---
+++ Source/ue5_gas_multiplayer/AbilitySystem/Abilities/AG_GameplayAbility.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbility.h"
+#include "AG_GameplayAbility.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE5_GAS_MULTIPLAYER_API UAG_GameplayAbility : public UGameplayAbility
+{
+    GENERATED_BODY()
+
+};
