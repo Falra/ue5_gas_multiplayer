44a0433a459f2324f14986cc8608d4c030807ca5
Valerii Diachenko <leriy.dyachenko@gmail.com>
2023-04-15 19:08:59 +0300

    #22 Add override for activation and ending ability (w/o any additional logic yet)

--- Source/ue5_gas_multiplayer/AbilitySystem/Abilities/AG_GameplayAbility.cpp
+++ Source/ue5_gas_multiplayer/AbilitySystem/Abilities/AG_GameplayAbility.cpp
@@ -2,3 +2,15 @@
 
 
 #include "AbilitySystem/Abilities/AG_GameplayAbility.h"
+
+void UAG_GameplayAbility::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo,
+    const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
+{
+    Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+}
+
+void UAG_GameplayAbility::EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo,
+    const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled)
+{
+    Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
+}
--- Source/ue5_gas_multiplayer/AbilitySystem/Abilities/AG_GameplayAbility.h
+++ Source/ue5_gas_multiplayer/AbilitySystem/Abilities/AG_GameplayAbility.h
@@ -6,12 +6,22 @@
 #include "Abilities/GameplayAbility.h"
 #include "AG_GameplayAbility.generated.h"
 
-/**
- * 
- */
 UCLASS()
 class UE5_GAS_MULTIPLAYER_API UAG_GameplayAbility : public UGameplayAbility
 {
     GENERATED_BODY()
 
+public:
+    virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo,
+        const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+
+    virtual void EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo,
+        const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled) override;
+
+protected:
+    UPROPERTY(EditDefaultsOnly, Category = "Effects")
+    TArray<TSubclassOf<UGameplayEffect>> OngoingEffectsToRemoveOnEnd;
+
+    UPROPERTY(EditDefaultsOnly, Category = "Effects")
+    TArray<TSubclassOf<UGameplayEffect>> OngoingEffectsToJustApplyOnStart;
 };
