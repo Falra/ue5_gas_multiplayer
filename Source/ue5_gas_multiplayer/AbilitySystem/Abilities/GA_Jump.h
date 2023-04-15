b3a30d1c9e45d88957b2accd781d685b8ef52e55
Valerii Diachenko <leriy.dyachenko@gmail.com>
2023-04-15 15:22:44 +0300

    #21 Add blank jump ability class and BP

---
+++ Source/ue5_gas_multiplayer/AbilitySystem/Abilities/GA_Jump.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/Abilities/AG_GameplayAbility.h"
+#include "GA_Jump.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE5_GAS_MULTIPLAYER_API UGA_Jump : public UAG_GameplayAbility
+{
+	GENERATED_BODY()
+	
+};
