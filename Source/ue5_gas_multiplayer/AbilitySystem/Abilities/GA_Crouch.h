ed23a9adfc106ed748f61c5e6146d1f0004024af
Valerii Diachenko <leriy.dyachenko@gmail.com>
2023-04-17 17:37:24 +0300

    #23 Add blank class and BP for crouch ability

---
+++ Source/ue5_gas_multiplayer/AbilitySystem/Abilities/GA_Crouch.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/Abilities/AG_GameplayAbility.h"
+#include "GA_Crouch.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE5_GAS_MULTIPLAYER_API UGA_Crouch : public UAG_GameplayAbility
+{
+    GENERATED_BODY()
+
+};
