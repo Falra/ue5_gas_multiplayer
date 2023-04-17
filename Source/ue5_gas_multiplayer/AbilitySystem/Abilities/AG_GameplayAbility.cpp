384bc7c8cd0e0ff4ce6a634624ad7125a1cd3a57
Valerii Diachenko <leriy.dyachenko@gmail.com>
2023-04-17 17:10:49 +0300

    #22 Add applying effects on start ability logic

--- Source/ue5_gas_multiplayer/AbilitySystem/Abilities/AG_GameplayAbility.cpp
+++ Source/ue5_gas_multiplayer/AbilitySystem/Abilities/AG_GameplayAbility.cpp
@@ -3,10 +3,32 @@
 
 #include "AbilitySystem/Abilities/AG_GameplayAbility.h"
 
+#include "AbilitySystemComponent.h"
+#include "AbilitySystemLog.h"
+
 void UAG_GameplayAbility::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo,
     const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
 {
     Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+
+    const FGameplayEffectContextHandle EffectContext = ActorInfo->AbilitySystemComponent->MakeEffectContext();
+    for (auto GameplayEffect : OngoingEffectsToJustApplyOnStart)
+    {
+        if (!GameplayEffect.Get())
+        {
+            continue;
+        }
+        FGameplayEffectSpecHandle SpecHandle = ActorInfo->AbilitySystemComponent->MakeOutgoingSpec(GameplayEffect, 1, EffectContext);
+        if (!SpecHandle.IsValid())
+        {
+            continue;
+        }
+        FActiveGameplayEffectHandle ActiveGEHandle = ActorInfo->AbilitySystemComponent->ApplyGameplayEffectSpecToSelf(*SpecHandle.Data.Get());
+        if (!ActiveGEHandle.WasSuccessfullyApplied())
+        {
+            ABILITY_LOG(Log, TEXT("Ability %s failed to apply startup effect %s"), *GetName(), *GetNameSafe(GameplayEffect));
+        }
+    }
 }
 
 void UAG_GameplayAbility::EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo,
