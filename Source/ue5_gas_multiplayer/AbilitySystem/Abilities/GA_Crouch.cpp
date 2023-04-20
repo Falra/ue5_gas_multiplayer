ea4eca050b5f481daca1ac1c9d8916ac817e08c3
Valerii Diachenko <leriy.dyachenko@gmail.com>
2023-04-20 15:26:27 +0300

    #23 Draft of ability constructor and methods override

--- Source/ue5_gas_multiplayer/AbilitySystem/Abilities/GA_Crouch.cpp
+++ Source/ue5_gas_multiplayer/AbilitySystem/Abilities/GA_Crouch.cpp
@@ -2,3 +2,26 @@
 
 
 #include "AbilitySystem/Abilities/GA_Crouch.h"
+
+UGA_Crouch::UGA_Crouch()
+{
+    
+}
+
+void UGA_Crouch::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo,
+    const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
+{
+    Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+}
+
+void UGA_Crouch::EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo,
+    const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled)
+{
+    Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
+}
+
+bool UGA_Crouch::CanActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo,
+    const FGameplayTagContainer* SourceTags, const FGameplayTagContainer* TargetTags, FGameplayTagContainer* OptionalRelevantTags) const
+{
+    return Super::CanActivateAbility(Handle, ActorInfo, SourceTags, TargetTags, OptionalRelevantTags);
+}
--- Source/ue5_gas_multiplayer/AbilitySystem/Abilities/GA_Crouch.h
+++ Source/ue5_gas_multiplayer/AbilitySystem/Abilities/GA_Crouch.h
@@ -14,4 +14,17 @@ class UE5_GAS_MULTIPLAYER_API UGA_Crouch : public UAG_GameplayAbility
 {
     GENERATED_BODY()
 
+public:
+    UGA_Crouch();
+    
+    virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo,
+        const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+
+    virtual void EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo,
+        const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled) override;
+
+    virtual bool CanActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo,
+        const FGameplayTagContainer* SourceTags, const FGameplayTagContainer* TargetTags,
+        FGameplayTagContainer* OptionalRelevantTags) const override;
+
 };
