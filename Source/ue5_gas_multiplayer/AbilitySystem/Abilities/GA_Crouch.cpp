9aafa19ac59655bd7635ad1e03f1695cc5ab613e
Valerii Diachenko <leriy.dyachenko@gmail.com>
2023-04-20 15:31:22 +0300

    #23 Add crouching and un-crouching logic

--- Source/ue5_gas_multiplayer/AbilitySystem/Abilities/GA_Crouch.cpp
+++ Source/ue5_gas_multiplayer/AbilitySystem/Abilities/GA_Crouch.cpp
@@ -3,25 +3,39 @@
 
 #include "AbilitySystem/Abilities/GA_Crouch.h"
 
+#include "GameFramework/Character.h"
+
 UGA_Crouch::UGA_Crouch()
 {
-    
+    NetExecutionPolicy = EGameplayAbilityNetExecutionPolicy::LocalPredicted;
+    InstancingPolicy = EGameplayAbilityInstancingPolicy::InstancedPerActor;
 }
 
 void UGA_Crouch::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo,
     const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
 {
     Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+        
+    ACharacter * Character = CastChecked<ACharacter>(ActorInfo->AvatarActor.Get());
+    Character->Crouch();
 }
 
 void UGA_Crouch::EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo,
     const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled)
 {
     Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
+    
+    ACharacter * Character = CastChecked<ACharacter>(ActorInfo->AvatarActor.Get());
+    Character->UnCrouch();
 }
 
 bool UGA_Crouch::CanActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo,
     const FGameplayTagContainer* SourceTags, const FGameplayTagContainer* TargetTags, FGameplayTagContainer* OptionalRelevantTags) const
 {
-    return Super::CanActivateAbility(Handle, ActorInfo, SourceTags, TargetTags, OptionalRelevantTags);
+    if (!Super::CanActivateAbility(Handle, ActorInfo, SourceTags, TargetTags, OptionalRelevantTags))
+    {
+        return false;
+    }
+    const auto* Character = CastChecked<ACharacter>(ActorInfo->AvatarActor.Get(), ECastCheckedType::NullAllowed); 
+    return Character && Character->CanCrouch();
 }
