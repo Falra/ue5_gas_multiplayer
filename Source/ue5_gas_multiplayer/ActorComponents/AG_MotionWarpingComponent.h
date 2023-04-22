6be9da0a7eb9be034180c0bc63847cc0872b6e45
Valerii Diachenko <leriy.dyachenko@gmail.com>
2023-04-22 14:55:48 +0300

    #26 Add blank motion warping component class

---
+++ Source/ue5_gas_multiplayer/ActorComponents/AG_MotionWarpingComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MotionWarpingComponent.h"
+#include "AG_MotionWarpingComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE5_GAS_MULTIPLAYER_API UAG_MotionWarpingComponent : public UMotionWarpingComponent
+{
+    GENERATED_BODY()
+
+};
