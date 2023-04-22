85409d047863cc6b090cabcbdd996a83e5d13e35
Valerii Diachenko <leriy.dyachenko@gmail.com>
2023-04-22 19:53:40 +0300

    #26 Add necessary properties to vault abilities

--- Source/ue5_gas_multiplayer/AbilitySystem/Abilities/GA_Vault.h
+++ Source/ue5_gas_multiplayer/AbilitySystem/Abilities/GA_Vault.h
@@ -25,4 +25,39 @@ public:
 
     virtual void EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo,
         const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled) override;
+
+protected:
+    UPROPERTY(EditDefaultsOnly, Category = HorizontalTrace)
+    float HorizontalTraceRadius = 30.0f;
+
+    UPROPERTY(EditDefaultsOnly, Category = HorizontalTrace)
+    float HorizontalTraceLength = 500.0f;
+
+    UPROPERTY(EditDefaultsOnly, Category = HorizontalTrace)
+    float HorizontalTraceStep = 30.0f;
+
+    UPROPERTY(EditDefaultsOnly, Category = HorizontalTrace)
+    uint8 HorizontalTraceCount = 5;
+
+    UPROPERTY(EditDefaultsOnly, Category = VerticalTrace)
+    float VerticalTraceRadius = 30.0f;
+
+    UPROPERTY(EditDefaultsOnly, Category = VerticalTrace)
+    float VerticalTraceStep = 30.0f;
+
+    UPROPERTY(EditDefaultsOnly)
+    TArray<TEnumAsByte<EObjectTypeQuery>> TraceObjectTypes;
+
+    UPROPERTY(EditDefaultsOnly)
+    UAnimMontage* VaultMontage = nullptr;
+
+    UPROPERTY(Transient)
+    class UAbilityTask_PlayMontageAndWait* VaultMontageTask = nullptr;
+    
+    FVector JumpLocation;
+    FVector JumpOverLocation;
+    
+    UPROPERTY(EditDefaultsOnly)
+    TArray<TEnumAsByte<ECollisionChannel>> CollisionChannelsToIgnore;
+    
 };
