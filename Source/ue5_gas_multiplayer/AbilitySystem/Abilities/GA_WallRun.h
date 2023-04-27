487a344c3135326fc903fb11408a8fdb30080443
Valerii Diachenko <leriy.dyachenko@gmail.com>
2023-04-27 20:54:00 +0300

    #27 Add blank classes for ability and task

---
+++ Source/ue5_gas_multiplayer/AbilitySystem/Abilities/GA_WallRun.h
@@ -0,0 +1,18 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AG_GameplayAbility.h"
+#include "GA_WallRun.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE5_GAS_MULTIPLAYER_API UGA_WallRun : public UAG_GameplayAbility
+{
+    GENERATED_BODY()
+
+
+};
---
+++ Source/ue5_gas_multiplayer/AbilitySystem/AbilityTasks/AbilityTask_WallRun.h
@@ -0,0 +1,18 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/Tasks/AbilityTask.h"
+#include "AbilityTask_WallRun.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE5_GAS_MULTIPLAYER_API UAbilityTask_WallRun : public UAbilityTask
+{
+    GENERATED_BODY()
+
+
+};
