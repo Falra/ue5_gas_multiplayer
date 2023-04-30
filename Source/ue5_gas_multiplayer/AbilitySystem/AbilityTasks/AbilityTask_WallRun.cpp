55d8ee047e9eedd0759a32e3b0e425f374bd797e
Valerii Diachenko <leriy.dyachenko@gmail.com>
2023-04-30 19:27:23 +0300

    #27 Add declaration of task overriding logic

--- Source/ue5_gas_multiplayer/AbilitySystem/AbilityTasks/AbilityTask_WallRun.cpp
+++ Source/ue5_gas_multiplayer/AbilitySystem/AbilityTasks/AbilityTask_WallRun.cpp
@@ -2,3 +2,24 @@
 
 
 #include "AbilityTask_WallRun.h"
+
+UAbilityTask_WallRun* UAbilityTask_WallRun::CreateWallRunTask(UGameplayAbility* OwningAbility, ACharacter* InCharacterOwner,
+    UCharacterMovementComponent* InCharacterMovement, TArray<TEnumAsByte<EObjectTypeQuery>> TraceObjectTypes)
+{
+    return nullptr;
+}
+
+void UAbilityTask_WallRun::Activate()
+{
+    Super::Activate();
+}
+
+void UAbilityTask_WallRun::OnDestroy(bool bInOwnerFinished)
+{
+    Super::OnDestroy(bInOwnerFinished);
+}
+
+void UAbilityTask_WallRun::TickTask(float DeltaTime)
+{
+    Super::TickTask(DeltaTime);
+}
--- Source/ue5_gas_multiplayer/AbilitySystem/AbilityTasks/AbilityTask_WallRun.h
+++ Source/ue5_gas_multiplayer/AbilitySystem/AbilityTasks/AbilityTask_WallRun.h
@@ -6,13 +6,35 @@
 #include "Abilities/Tasks/AbilityTask.h"
 #include "AbilityTask_WallRun.generated.h"
 
-/**
- * 
- */
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnWallRunWallSideDeterminedDelegate, bool, bLeftSide);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnWallRunFinishedDelegate);
+
+class ACharacter;
+class UCharacterMovementComponent;
+
 UCLASS()
 class UE5_GAS_MULTIPLAYER_API UAbilityTask_WallRun : public UAbilityTask
 {
     GENERATED_BODY()
 
+public:
+    UPROPERTY(BlueprintAssignable)
+    FOnWallRunFinishedDelegate OnWallRunFinished;
 
+    UPROPERTY(BlueprintAssignable)
+    FOnWallRunWallSideDeterminedDelegate OnWallRunWallSideDetermined;
+
+    UFUNCTION(BlueprintCallable, Category = "Ability|Tasks", meta = (HidePin = "OwningAbility", DefaultToSelf = "OwningAbility"))
+    static UAbilityTask_WallRun* CreateWallRunTask(UGameplayAbility* OwningAbility, ACharacter* InCharacterOwner,
+        UCharacterMovementComponent* InCharacterMovement, TArray<TEnumAsByte<EObjectTypeQuery>> TraceObjectTypes);
+
+    virtual void Activate() override;
+
+    virtual void OnDestroy(bool bInOwnerFinished) override;
+
+    virtual void TickTask(float DeltaTime) override;
+
+protected:
+    
 };
