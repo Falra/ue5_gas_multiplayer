588d2ccd734720bf66da3469270d58029958e725
Valerii Diachenko <leriy.dyachenko@gmail.com>
2023-05-02 21:03:41 +0300

    #27 Add draft overrides for wall run ability

--- Source/ue5_gas_multiplayer/AbilitySystem/Abilities/GA_WallRun.cpp
+++ Source/ue5_gas_multiplayer/AbilitySystem/Abilities/GA_WallRun.cpp
@@ -2,3 +2,35 @@
 
 
 #include "GA_WallRun.h"
+
+UGA_WallRun::UGA_WallRun()
+{
+}
+
+void UGA_WallRun::OnGiveAbility(const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilitySpec& Spec)
+{
+    Super::OnGiveAbility(ActorInfo, Spec);
+}
+
+void UGA_WallRun::OnRemoveAbility(const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilitySpec& Spec)
+{
+    Super::OnRemoveAbility(ActorInfo, Spec);
+}
+
+bool UGA_WallRun::CanActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo,
+    const FGameplayTagContainer* SourceTags, const FGameplayTagContainer* TargetTags, FGameplayTagContainer* OptionalRelevantTags) const
+{
+    return Super::CanActivateAbility(Handle, ActorInfo, SourceTags, TargetTags, OptionalRelevantTags);
+}
+
+void UGA_WallRun::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo,
+    const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
+{
+    Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+}
+
+void UGA_WallRun::EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo,
+    const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled)
+{
+    Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
+}
--- Source/ue5_gas_multiplayer/AbilitySystem/Abilities/GA_WallRun.h
+++ Source/ue5_gas_multiplayer/AbilitySystem/Abilities/GA_WallRun.h
@@ -6,13 +6,27 @@
 #include "AG_GameplayAbility.h"
 #include "GA_WallRun.generated.h"
 
-/**
- * 
- */
+class UAbilityTask_WallRun;
+
 UCLASS()
 class UE5_GAS_MULTIPLAYER_API UGA_WallRun : public UAG_GameplayAbility
 {
     GENERATED_BODY()
 
+public:
+    UGA_WallRun();
 
+    virtual void OnGiveAbility(const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilitySpec& Spec) override;
+
+    virtual void OnRemoveAbility(const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilitySpec& Spec) override;
+
+    virtual bool CanActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo,
+        const FGameplayTagContainer* SourceTags, const FGameplayTagContainer* TargetTags,
+        FGameplayTagContainer* OptionalRelevantTags) const override;
+
+    virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo,
+        const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+
+    virtual void EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo,
+        const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled) override;
 };
