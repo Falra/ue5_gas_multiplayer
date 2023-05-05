e50406df5218c126aa6d38891a7b191cec999cab
Valerii Diachenko <leriy.dyachenko@gmail.com>
2023-05-05 09:00:26 +0300

    #30 Add blank class for BP function library

---
+++ Source/ue5_gas_multiplayer/ActionGameStatics.h
@@ -0,0 +1,18 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "ActionGameStatics.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE5_GAS_MULTIPLAYER_API UActionGameStatics : public UBlueprintFunctionLibrary
+{
+    GENERATED_BODY()
+
+
+};
