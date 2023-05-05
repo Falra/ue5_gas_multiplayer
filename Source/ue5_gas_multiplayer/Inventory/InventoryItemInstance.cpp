7884b60b9653ed485d3808772afb5c1e76737491
Valerii Diachenko <leriy.dyachenko@gmail.com>
2023-05-05 20:24:28 +0300

    #31 Basic item logic w/ equipped flag

--- Source/ue5_gas_multiplayer/Inventory/InventoryItemInstance.cpp
+++ Source/ue5_gas_multiplayer/Inventory/InventoryItemInstance.cpp
@@ -2,3 +2,24 @@
 
 
 #include "InventoryItemInstance.h"
+
+#include "Net/UnrealNetwork.h"
+
+void UInventoryItemInstance::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+    Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+    DOREPLIFETIME(UInventoryItemInstance, bEquipped);
+}
+
+void UInventoryItemInstance::OnRep_Equipped()
+{
+    if (bEquipped)
+    {
+        OnEquipped();
+    }
+    else
+    {
+        OnUnequipped();
+    }
+}
--- Source/ue5_gas_multiplayer/Inventory/InventoryItemInstance.h
+++ Source/ue5_gas_multiplayer/Inventory/InventoryItemInstance.h
@@ -6,10 +6,25 @@
 #include "UObject/Object.h"
 #include "InventoryItemInstance.generated.h"
 
+class UItemStaticData;
+
 UCLASS()
 class UE5_GAS_MULTIPLAYER_API UInventoryItemInstance : public UObject
 {
     GENERATED_BODY()
 
+public:
+    virtual bool IsSupportedForNetworking() const override { return true; }
 
+    UPROPERTY(Replicated)
+    TSubclassOf<UItemStaticData> ItemStaticDataClass;
+
+    UPROPERTY(ReplicatedUsing = OnRep_Equipped)
+    bool bEquipped = false;
+
+    UFUNCTION()
+    void OnRep_Equipped();
+
+    virtual void OnEquipped() {}
+    virtual void OnUnequipped() {}
 };
