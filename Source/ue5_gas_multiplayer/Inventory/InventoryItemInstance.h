eb995a2a51008dd173ce2a1ec38277c3179e107c
Valerii Diachenko <leriy.dyachenko@gmail.com>
2023-05-05 20:12:57 +0300

    #31 Add blank class for inventory item

---
+++ Source/ue5_gas_multiplayer/Inventory/InventoryItemInstance.h
@@ -0,0 +1,15 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "InventoryItemInstance.generated.h"
+
+UCLASS()
+class UE5_GAS_MULTIPLAYER_API UInventoryItemInstance : public UObject
+{
+    GENERATED_BODY()
+
+
+};
