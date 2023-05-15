df8ab9689a8cbee4a9a643fd702eee971cf84940
Valerii Diachenko <leriy.dyachenko@gmail.com>
2023-05-15 19:31:46 +0300

    #36 Add blank weapon item actor class

---
+++ Source/ue5_gas_multiplayer/Inventory/ItemActors/WeaponItemActor.cpp
@@ -0,0 +1,18 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "WeaponItemActor.h"
+
+AWeaponItemActor::AWeaponItemActor()
+{
+    PrimaryActorTick.bCanEverTick = true;
+}
+
+void AWeaponItemActor::BeginPlay()
+{
+    Super::BeginPlay();
+}
+
+void AWeaponItemActor::Tick(float DeltaTime)
+{
+    Super::Tick(DeltaTime);
+}
---
+++ Source/ue5_gas_multiplayer/Inventory/ItemActors/WeaponItemActor.h
@@ -0,0 +1,22 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Actors/ItemActor.h"
+#include "WeaponItemActor.generated.h"
+
+UCLASS()
+class UE5_GAS_MULTIPLAYER_API AWeaponItemActor : public AItemActor
+{
+    GENERATED_BODY()
+
+public:
+    AWeaponItemActor();
+
+protected:
+    virtual void BeginPlay() override;
+
+public:
+    virtual void Tick(float DeltaTime) override;
+};
