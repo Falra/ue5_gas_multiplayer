fd0c358c05dbc09eb641c19ae989b9c1021a007c
Valerii Diachenko <leriy.dyachenko@gmail.com>
2023-05-19 20:23:23 +0300

    #37 Add blank class for base inventory ability

---
+++ Source/ue5_gas_multiplayer/AbilitySystem/Abilities/GA_InventoryAbility.h
@@ -0,0 +1,15 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AG_GameplayAbility.h"
+#include "GA_InventoryAbility.generated.h"
+
+UCLASS()
+class UE5_GAS_MULTIPLAYER_API UGA_InventoryAbility : public UAG_GameplayAbility
+{
+    GENERATED_BODY()
+
+
+};
