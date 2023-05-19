46be9035da6ccbffea1fffdaa297ac30489e95ba
Valerii Diachenko <leriy.dyachenko@gmail.com>
2023-05-19 20:27:44 +0300

    #37 Add blank class for anim notify used with gameplay events

---
+++ Source/ue5_gas_multiplayer/AnimNotifies/AG_AnimNotify_GameplayEvent.h
@@ -0,0 +1,15 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotify.h"
+#include "AG_AnimNotify_GameplayEvent.generated.h"
+
+UCLASS()
+class UE5_GAS_MULTIPLAYER_API UAG_AnimNotify_GameplayEvent : public UAnimNotify
+{
+    GENERATED_BODY()
+
+
+};
