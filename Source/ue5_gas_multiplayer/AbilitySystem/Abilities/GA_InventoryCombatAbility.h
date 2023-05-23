59ca6c68bbd95f28d9ef0ddf70931da4a7e8cc1f
Valerii Diachenko <leriy.dyachenko@gmail.com>
2023-05-23 20:40:17 +0300

    #40 Add blank base for combat ability

---
+++ Source/ue5_gas_multiplayer/AbilitySystem/Abilities/GA_InventoryCombatAbility.h
@@ -0,0 +1,18 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GA_InventoryAbility.h"
+#include "GA_InventoryCombatAbility.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE5_GAS_MULTIPLAYER_API UGA_InventoryCombatAbility : public UGA_InventoryAbility
+{
+    GENERATED_BODY()
+
+
+};
