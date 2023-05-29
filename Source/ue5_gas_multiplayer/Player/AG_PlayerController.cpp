493c2ecc3ebd78929b75fd7af8d738e142d18b39
Valerii Diachenko <leriy.dyachenko@gmail.com>
2023-05-29 08:33:55 +0300

    #43 Add blank player controller class

---
+++ Source/ue5_gas_multiplayer/Player/AG_PlayerController.cpp
@@ -0,0 +1,9 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AG_PlayerController.h"
+
+
+AAG_PlayerController::AAG_PlayerController()
+{
+}
---
+++ Source/ue5_gas_multiplayer/Player/AG_PlayerController.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "AG_PlayerController.generated.h"
+
+UCLASS()
+class UE5_GAS_MULTIPLAYER_API AAG_PlayerController : public APlayerController
+{
+    GENERATED_BODY()
+
+public:
+    AAG_PlayerController();
+};
