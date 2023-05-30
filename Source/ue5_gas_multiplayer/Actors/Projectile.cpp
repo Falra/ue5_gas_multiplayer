65c5893cecd67117995c1f6bb9c8eb0ad1b8c4a3
Valerii Diachenko <leriy.dyachenko@gmail.com>
2023-05-30 09:30:43 +0300

    #44 Add blank projectile class

---
+++ Source/ue5_gas_multiplayer/Actors/Projectile.cpp
@@ -0,0 +1,20 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Projectile.h"
+
+AProjectile::AProjectile()
+{
+    PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AProjectile::BeginPlay()
+{
+    Super::BeginPlay();
+}
+
+void AProjectile::Tick(float DeltaTime)
+{
+    Super::Tick(DeltaTime);
+}
---
+++ Source/ue5_gas_multiplayer/Actors/Projectile.h
@@ -0,0 +1,23 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Projectile.generated.h"
+
+UCLASS()
+class UE5_GAS_MULTIPLAYER_API AProjectile : public AActor
+{
+    GENERATED_BODY()
+
+public:
+    AProjectile();
+
+protected:
+    virtual void BeginPlay() override;
+
+public:
+    virtual void Tick(float DeltaTime) override;
+
+};
