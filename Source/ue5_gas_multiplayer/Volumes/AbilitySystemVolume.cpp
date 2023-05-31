034c30fdd2f7e90dd21d8ddf199677bfd1ba6dc7
Valerii Diachenko <leriy.dyachenko@gmail.com>
2023-05-31 09:01:51 +0300

    #45 Add blank class for volume with Ability system

---
+++ Source/ue5_gas_multiplayer/Volumes/AbilitySystemVolume.cpp
@@ -0,0 +1,20 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AbilitySystemVolume.h"
+
+
+AAbilitySystemVolume::AAbilitySystemVolume()
+{
+    PrimaryActorTick.bCanEverTick = true;
+}
+
+void AAbilitySystemVolume::BeginPlay()
+{
+    Super::BeginPlay();
+}
+
+void AAbilitySystemVolume::Tick(float DeltaTime)
+{
+    Super::Tick(DeltaTime);
+}
---
+++ Source/ue5_gas_multiplayer/Volumes/AbilitySystemVolume.h
@@ -0,0 +1,23 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PhysicsVolume.h"
+#include "AbilitySystemVolume.generated.h"
+
+UCLASS()
+class UE5_GAS_MULTIPLAYER_API AAbilitySystemVolume : public APhysicsVolume
+{
+    GENERATED_BODY()
+
+public:
+    AAbilitySystemVolume();
+
+protected:
+    virtual void BeginPlay() override;
+
+public:
+    virtual void Tick(float DeltaTime) override;
+    
+};
