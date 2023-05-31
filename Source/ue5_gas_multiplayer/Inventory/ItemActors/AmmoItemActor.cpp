d703d33005a7ca3ff9ad03af2b597f940beb4a3b
Valerii Diachenko <leriy.dyachenko@gmail.com>
2023-05-31 20:27:03 +0300

    #48 Add blank ammo item actor class

---
+++ Source/ue5_gas_multiplayer/Inventory/ItemActors/AmmoItemActor.cpp
@@ -0,0 +1,10 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AmmoItemActor.h"
+
+
+AAmmoItemActor::AAmmoItemActor()
+{
+    
+}
---
+++ Source/ue5_gas_multiplayer/Inventory/ItemActors/AmmoItemActor.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Actors/ItemActor.h"
+#include "AmmoItemActor.generated.h"
+
+UCLASS()
+class UE5_GAS_MULTIPLAYER_API AAmmoItemActor : public AItemActor
+{
+    GENERATED_BODY()
+
+public:
+    AAmmoItemActor();
+};
