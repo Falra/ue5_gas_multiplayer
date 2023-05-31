f882ff5e8b8ab0935e2bdf65ab95a08161010bab
Valerii Diachenko <leriy.dyachenko@gmail.com>
2023-05-31 09:34:30 +0300

    #45 Implement logic for exiting volume

--- Source/ue5_gas_multiplayer/Volumes/AbilitySystemVolume.cpp
+++ Source/ue5_gas_multiplayer/Volumes/AbilitySystemVolume.cpp
@@ -73,4 +73,49 @@ void AAbilitySystemVolume::ActorEnteredVolume(AActor* Other)
 void AAbilitySystemVolume::ActorLeavingVolume(AActor* Other)
 {
     Super::ActorLeavingVolume(Other);
+    
+    if (!HasAuthority() || !EnteredActorsInfoMap.Contains(Other))
+    {
+        return;
+    }
+    if (auto* ASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(Other))
+    {
+        if (EnteredActorsInfoMap.Contains(Other))
+        {
+            for (const auto GameplayEffectHandle : EnteredActorsInfoMap[Other].AppliedEffects)
+            {
+                ASC->RemoveActiveGameplayEffect(GameplayEffectHandle);
+            }
+
+            for (const auto GameplayAbilityHandle : EnteredActorsInfoMap[Other].AppliedAbilities)
+            {
+                ASC->ClearAbility(GameplayAbilityHandle);
+            }
+
+            EnteredActorsInfoMap.Remove(Other);
+        }
+
+        FGameplayEffectContextHandle EffectContext = ASC->MakeEffectContext();
+        EffectContext.AddInstigator(Other, Other);
+        
+        for (const auto Effect : OnExitEffectsToApply)
+        {
+            FGameplayEffectSpecHandle SpecHandle = ASC->MakeOutgoingSpec(Effect, 1, EffectContext);
+
+            if (!SpecHandle.IsValid())
+            {
+                continue;
+            }
+            
+            ASC->ApplyGameplayEffectSpecToSelf(*SpecHandle.Data.Get());
+        }
+        
+        for (const auto EventTag : GameplayEventsToSendOnExit)
+        {
+            FGameplayEventData EventData;
+            EventData.EventTag = EventTag;
+
+            UAbilitySystemBlueprintLibrary::SendGameplayEventToActor(Other, EventTag, EventData);
+        }
+    }
 }
