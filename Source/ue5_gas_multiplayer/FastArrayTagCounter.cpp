b9cfe2f08d6591697021127e9f9172d3556d83b2
Valerii Diachenko <leriy.dyachenko@gmail.com>
2023-05-31 20:59:08 +0300

    #48 Stacking logic for ammo via fast array of tags and count

---
+++ Source/ue5_gas_multiplayer/FastArrayTagCounter.cpp
@@ -0,0 +1,49 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FastArrayTagCounter.h"
+
+int32 FFastArrayTagCounter::GetTagCount(FGameplayTag Tag) const
+{
+    for (auto ItemIter = TagArray.CreateConstIterator(); ItemIter; ++ItemIter)
+    {
+        const FFastArrayTagCounterRecord& Item = *ItemIter;
+        if (Item.Tag == Tag)
+        {
+            return Item.Count;
+        }
+    }
+    return 0;
+}
+
+void FFastArrayTagCounter::AddTagCount(FGameplayTag Tag, int32 Count)
+{
+    for (auto ItemIter = TagArray.CreateIterator(); ItemIter; ++ItemIter)
+    {
+        FFastArrayTagCounterRecord& Item = *ItemIter;
+        if (Item.Tag == Tag)
+        {
+            Item.Count += Count;
+            if (Item.Count <= 0)
+            {
+                ItemIter.RemoveCurrent();
+                MarkArrayDirty();
+            }
+            else
+            {
+                MarkItemDirty(Item);
+            }
+            return;
+        }
+    }
+    
+    FFastArrayTagCounterRecord& Item = TagArray.AddDefaulted_GetRef();
+    Item.Tag = Tag;
+    Item.Count = Count;
+    MarkItemDirty(Item);
+}
+
+const TArray<FFastArrayTagCounterRecord>& FFastArrayTagCounter::GetTagArray() const
+{
+    return TagArray;
+}
---
+++ Source/ue5_gas_multiplayer/FastArrayTagCounter.h
@@ -0,0 +1,52 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "Net/Serialization/FastArraySerializer.h"
+#include "UObject/Object.h"
+#include "FastArrayTagCounter.generated.h"
+
+USTRUCT(BlueprintType)
+struct FFastArrayTagCounterRecord : public FFastArraySerializerItem
+{
+    GENERATED_BODY()
+    
+    UPROPERTY()
+    FGameplayTag Tag;
+
+    UPROPERTY()
+    int32 Count = 0;
+};
+
+USTRUCT(BlueprintType)
+struct FFastArrayTagCounter : public FFastArraySerializer
+{
+    GENERATED_BODY()
+    
+    bool NetDeltaSerialize(FNetDeltaSerializeInfo& DeltaParams)
+    {
+        return FastArrayDeltaSerialize<FFastArrayTagCounterRecord, FFastArrayTagCounter>( TagArray, DeltaParams, *this );
+    }
+
+    int32 GetTagCount(FGameplayTag Tag) const;
+    
+    void AddTagCount(FGameplayTag Tag, int32 Count);
+    
+    const TArray<FFastArrayTagCounterRecord>& GetTagArray() const;
+    
+protected:
+
+    UPROPERTY()
+    TArray<FFastArrayTagCounterRecord> TagArray;
+};
+
+template<>
+struct TStructOpsTypeTraits<FFastArrayTagCounter> : public TStructOpsTypeTraitsBase2<FFastArrayTagCounter>
+{
+    enum
+    {
+        WithNetDeltaSerializer = true,
+    };
+};
